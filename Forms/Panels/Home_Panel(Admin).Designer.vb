91b7a9c4cdeb1ffbdd0b4813041abe630ad36a50
Shan2411 <shandavid2411@gmail.com>
2025-10-22 23:45:32 +0800

    nope

--- Forms/Panels/Home_Panel(Admin).Designer.vb
+++ Forms/Panels/Home_Panel(Admin).Designer.vb
@@ -22,9 +22,16 @@ Partial Class Home_Panel_Admin_
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'Home_Panel_Admin_
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(811, 488)
+        Me.Name = "Home_Panel_Admin_"
         Me.Text = "Home_Panel_Admin_"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
---
+++ Forms/Panels/Librarian_homepage.vb
@@ -0,0 +1,13 @@
+﻿Public Class Librarian_homepage
+    Private Sub Guna2Button2_Click(sender As Object, e As EventArgs) Handles Guna2Button2.Click
+
+    End Sub
+
+    Private Sub Guna2Button3_Click(sender As Object, e As EventArgs) Handles Guna2Button3.Click
+
+    End Sub
+
+    Private Sub TableLayoutPanel3_Paint(sender As Object, e As PaintEventArgs) Handles TableLayoutPanel3.Paint
+
+    End Sub
+End Class
