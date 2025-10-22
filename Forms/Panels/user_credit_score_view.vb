8854ab30b504b880d4a1f09352f8d267a8c7bf6b
Shan2411 <shandavid2411@gmail.com>
2025-10-23 01:38:59 +0800

    added form for user credit score view for admin/staff side

--- Forms/Panels/user_credit_score_view.vb
+++ Forms/Panels/user_credit_score_view.vb
@@ -1,9 +1,9 @@
-﻿Public Class user_request_view_profile
+﻿Public Class user_credit_score_view
     Private Sub Guna2Button1_Click(sender As Object, e As EventArgs) Handles Guna2Button1.Click
         Me.Close()
     End Sub
 
-    Private Sub Guna2Panel5_Paint(sender As Object, e As PaintEventArgs) Handles Guna2Panel5.Paint
+    Private Sub Guna2Panel5_Paint(sender As Object, e As PaintEventArgs)
 
     End Sub
 
@@ -16,6 +16,14 @@
     End Sub
 
     Private Sub user_request_view_profile_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Guna2PictureBox1_Click(sender As Object, e As EventArgs) Handles Guna2PictureBox1.Click
+
+    End Sub
+
+    Private Sub Guna2CircleProgressBar1_ValueChanged(sender As Object, e As EventArgs) Handles Guna2CircleProgressBar1.ValueChanged
 
     End Sub
 End Class
---
+++ Forms/Panels/user_req_view_profile.vb
@@ -0,0 +1,9 @@
+﻿Public Class user_req_view_profile
+    Private Sub Guna2PictureBox1_Click(sender As Object, e As EventArgs) Handles Guna2PictureBox1.Click
+
+    End Sub
+
+    Private Sub Guna2Button1_Click(sender As Object, e As EventArgs) Handles Guna2Button1.Click
+        Me.Close()
+    End Sub
+End Class
