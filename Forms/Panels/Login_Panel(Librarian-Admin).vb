e9c38af4adaf8ea0e74395a52776726d9bc3a357
Degenerate1456 <186584961+Huuujji@users.noreply.github.com>
2025-10-23 07:46:47 +0800

    Jestine UI

--- Forms/Panels/Login_Panel(Librarian-Admin).vb
+++ Forms/Panels/Login_Panel(Librarian-Admin).vb
@@ -1,3 +1,13 @@
 ﻿Public Class Login_Panel_Librarian_Admin_
+    Private Sub Guna2Button1_Click(sender As Object, e As EventArgs) Handles Guna2Button1.Click
 
+    End Sub
+
+    Private Sub Guna2Button2_Click(sender As Object, e As EventArgs) Handles btn_login.Click
+
+    End Sub
+
+    Private Sub Guna2TextBox1_TextChanged(sender As Object, e As EventArgs) Handles Guna2TextBox1.TextChanged
+
+    End Sub
 End Class
