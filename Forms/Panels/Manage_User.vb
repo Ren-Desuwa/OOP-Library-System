00a3fa74a95a37dcedb078a5b40fc7d7dcbf6126
Shan2411 <shandavid2411@gmail.com>
2025-10-24 17:29:49 +0800

    add forms

---
+++ Forms/Panels/Manage_User.vb
@@ -0,0 +1,27 @@
+﻿Public Class Manage_User
+    Private Sub Guna2Button3_Click(sender As Object, e As EventArgs) Handles Guna2Button3.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Manage_User_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Guna2CircleProgressBar1_ValueChanged(sender As Object, e As EventArgs) Handles Guna2CircleProgressBar1.ValueChanged
+
+    End Sub
+
+    Private Sub Guna2Button1_Click(sender As Object, e As EventArgs) Handles Guna2Button1.Click
+        Dim usercreditScoreViewForm As New user_credit_score_view()
+        usercreditScoreViewForm.Show()
+        Me.Close()
+    End Sub
+End Class
