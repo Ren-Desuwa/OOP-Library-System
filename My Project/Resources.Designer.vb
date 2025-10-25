c01cf558cd236d2a707a82a0bf9caa9fd56ae580
Degenerate1456 <186584961+Huuujji@users.noreply.github.com>
2025-10-25 16:16:41 +0800

    Fixed Library Info and added one book cover

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -93,6 +93,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property the_adventure() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("the_adventure", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property Ucc_Logo_NoBG() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Ucc_Logo_NoBG", resourceCulture)
