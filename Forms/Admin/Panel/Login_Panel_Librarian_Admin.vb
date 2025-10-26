c8b9639a217c4ef8eccc747f68d5d0729ae54e26
Degenerate1456 <186584961+Huuujji@users.noreply.github.com>
2025-10-26 18:09:45 +0800

    fixed merges

--- Forms/Admin/Panel/Login_Panel_Librarian_Admin.vb
+++ Forms/Admin/Panel/Login_Panel_Librarian_Admin.vb
@@ -48,11 +48,11 @@
 
             ' --- UPDATED LINE ---
             ' No longer passes "Me"
-<<<<<<<< HEAD:Forms/Admin/Login/Login_Panel(Librarian-Admin).vb
+
             'Dim homePanel As New Home_Panel_Librarian_Admin_(loggedInUsername, userRole)
-========
+
             Dim homePanel As New Home_Panel_Admin_Librarian(loggedInUsername, userRole)
->>>>>>>> UI-MAIN:Forms/Admin/Panel/Login_Panel_Librarian_Admin.vb
+
 
             'homePanel.Show()
 
