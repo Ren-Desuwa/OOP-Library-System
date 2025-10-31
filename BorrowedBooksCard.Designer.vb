e976bae6fe370666acc5842e6d6240f83275c00d
Myasora <owenmiranda042016@gmail.com>
2025-10-31 14:29:24 +0800

    done v4 (borrowedbooks)

--- BorrowedBooksCard.Designer.vb
+++ BorrowedBooksCard.Designer.vb
@@ -24,15 +24,15 @@ Partial Class BorrowedBooksCard
     Private Sub InitializeComponent()
         Me.Guna2Panel1 = New Guna.UI2.WinForms.Guna2Panel()
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
-        Me.Guna2PictureBox1 = New Guna.UI2.WinForms.Guna2PictureBox()
-        Me.lblDueDate = New System.Windows.Forms.Label()
-        Me.lblBorrowedDate = New System.Windows.Forms.Label()
-        Me.lblBookTitle = New System.Windows.Forms.Label()
         Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.lblBookTitle = New System.Windows.Forms.Label()
+        Me.lblBorrowedDate = New System.Windows.Forms.Label()
+        Me.lblDueDate = New System.Windows.Forms.Label()
+        Me.Guna2PictureBox1 = New Guna.UI2.WinForms.Guna2PictureBox()
         Me.Guna2Panel1.SuspendLayout()
         Me.TableLayoutPanel1.SuspendLayout()
-        CType(Me.Guna2PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.TableLayoutPanel2.SuspendLayout()
+        CType(Me.Guna2PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Guna2Panel1
@@ -42,11 +42,11 @@ Partial Class BorrowedBooksCard
         Me.Guna2Panel1.BorderThickness = 2
         Me.Guna2Panel1.Controls.Add(Me.TableLayoutPanel1)
         Me.Guna2Panel1.FillColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
-        Me.Guna2Panel1.Location = New System.Drawing.Point(10, 10)
+        Me.Guna2Panel1.Location = New System.Drawing.Point(0, 0)
         Me.Guna2Panel1.Margin = New System.Windows.Forms.Padding(10)
         Me.Guna2Panel1.Name = "Guna2Panel1"
         Me.Guna2Panel1.Padding = New System.Windows.Forms.Padding(10)
-        Me.Guna2Panel1.Size = New System.Drawing.Size(706, 130)
+        Me.Guna2Panel1.Size = New System.Drawing.Size(726, 150)
         Me.Guna2Panel1.TabIndex = 0
         '
         'TableLayoutPanel1
@@ -64,54 +64,8 @@ Partial Class BorrowedBooksCard
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
         Me.TableLayoutPanel1.RowCount = 1
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(686, 110)
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(706, 130)
         Me.TableLayoutPanel1.TabIndex = 0
-        '
-        'Guna2PictureBox1
-        '
-        Me.Guna2PictureBox1.ImageRotate = 0!
-        Me.Guna2PictureBox1.Location = New System.Drawing.Point(3, 3)
-        Me.Guna2PictureBox1.Name = "Guna2PictureBox1"
-        Me.Guna2PictureBox1.Size = New System.Drawing.Size(128, 104)
-        Me.Guna2PictureBox1.TabIndex = 0
-        Me.Guna2PictureBox1.TabStop = False
-        '
-        'lblDueDate
-        '
-        Me.lblDueDate.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
-        Me.lblDueDate.AutoSize = True
-        Me.lblDueDate.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblDueDate.Location = New System.Drawing.Point(272, 86)
-        Me.lblDueDate.Margin = New System.Windows.Forms.Padding(0)
-        Me.lblDueDate.Name = "lblDueDate"
-        Me.lblDueDate.Size = New System.Drawing.Size(74, 18)
-        Me.lblDueDate.TabIndex = 2
-        Me.lblDueDate.Text = "Due Date:"
-        Me.lblDueDate.TextAlign = System.Drawing.ContentAlignment.BottomLeft
-        '
-        'lblBorrowedDate
-        '
-        Me.lblBorrowedDate.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
-        Me.lblBorrowedDate.AutoSize = True
-        Me.lblBorrowedDate.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblBorrowedDate.Location = New System.Drawing.Point(0, 86)
-        Me.lblBorrowedDate.Margin = New System.Windows.Forms.Padding(0)
-        Me.lblBorrowedDate.Name = "lblBorrowedDate"
-        Me.lblBorrowedDate.Size = New System.Drawing.Size(108, 18)
-        Me.lblBorrowedDate.TabIndex = 1
-        Me.lblBorrowedDate.Text = "Borrowed Date"
-        Me.lblBorrowedDate.TextAlign = System.Drawing.ContentAlignment.BottomLeft
-        '
-        'lblBookTitle
-        '
-        Me.lblBookTitle.Anchor = System.Windows.Forms.AnchorStyles.Left
-        Me.lblBookTitle.AutoSize = True
-        Me.lblBookTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblBookTitle.Location = New System.Drawing.Point(3, 18)
-        Me.lblBookTitle.Name = "lblBookTitle"
-        Me.lblBookTitle.Size = New System.Drawing.Size(149, 31)
-        Me.lblBookTitle.TabIndex = 0
-        Me.lblBookTitle.Text = "One Piece"
         '
         'TableLayoutPanel2
         '
@@ -122,13 +76,60 @@ Partial Class BorrowedBooksCard
         Me.TableLayoutPanel2.Controls.Add(Me.lblBorrowedDate, 0, 1)
         Me.TableLayoutPanel2.Controls.Add(Me.lblDueDate, 1, 1)
         Me.TableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TableLayoutPanel2.Location = New System.Drawing.Point(139, 3)
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(143, 3)
         Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
         Me.TableLayoutPanel2.RowCount = 2
         Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 65.38461!))
         Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34.61538!))
-        Me.TableLayoutPanel2.Size = New System.Drawing.Size(544, 104)
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(560, 124)
         Me.TableLayoutPanel2.TabIndex = 1
+        '
+        'lblBookTitle
+        '
+        Me.lblBookTitle.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.lblBookTitle.AutoSize = True
+        Me.lblBookTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBookTitle.Location = New System.Drawing.Point(3, 25)
+        Me.lblBookTitle.Name = "lblBookTitle"
+        Me.lblBookTitle.Size = New System.Drawing.Size(149, 31)
+        Me.lblBookTitle.TabIndex = 0
+        Me.lblBookTitle.Text = "One Piece"
+        '
+        'lblBorrowedDate
+        '
+        Me.lblBorrowedDate.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lblBorrowedDate.AutoSize = True
+        Me.lblBorrowedDate.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBorrowedDate.Location = New System.Drawing.Point(0, 106)
+        Me.lblBorrowedDate.Margin = New System.Windows.Forms.Padding(0)
+        Me.lblBorrowedDate.Name = "lblBorrowedDate"
+        Me.lblBorrowedDate.Size = New System.Drawing.Size(108, 18)
+        Me.lblBorrowedDate.TabIndex = 1
+        Me.lblBorrowedDate.Text = "Borrowed Date"
+        Me.lblBorrowedDate.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        '
+        'lblDueDate
+        '
+        Me.lblDueDate.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lblDueDate.AutoSize = True
+        Me.lblDueDate.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDueDate.Location = New System.Drawing.Point(280, 106)
+        Me.lblDueDate.Margin = New System.Windows.Forms.Padding(0)
+        Me.lblDueDate.Name = "lblDueDate"
+        Me.lblDueDate.Size = New System.Drawing.Size(74, 18)
+        Me.lblDueDate.TabIndex = 2
+        Me.lblDueDate.Text = "Due Date:"
+        Me.lblDueDate.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        '
+        'Guna2PictureBox1
+        '
+        Me.Guna2PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Guna2PictureBox1.ImageRotate = 0!
+        Me.Guna2PictureBox1.Location = New System.Drawing.Point(3, 3)
+        Me.Guna2PictureBox1.Name = "Guna2PictureBox1"
+        Me.Guna2PictureBox1.Size = New System.Drawing.Size(134, 124)
+        Me.Guna2PictureBox1.TabIndex = 0
+        Me.Guna2PictureBox1.TabStop = False
         '
         'BorrowedBooksCard
         '
@@ -139,9 +140,9 @@ Partial Class BorrowedBooksCard
         Me.Size = New System.Drawing.Size(726, 150)
         Me.Guna2Panel1.ResumeLayout(False)
         Me.TableLayoutPanel1.ResumeLayout(False)
-        CType(Me.Guna2PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.TableLayoutPanel2.ResumeLayout(False)
         Me.TableLayoutPanel2.PerformLayout()
+        CType(Me.Guna2PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
--- BorrowedBooksCard.vb
+++ BorrowedBooksCard.vb
@@ -1,5 +1,5 @@
 ﻿Public Class BorrowedBooksCard
-    ' Optional: expose properties para sa data
+
     Public Property BookTitle As String
         Get
             Return lblBookTitle.Text
@@ -26,4 +26,49 @@
             lblDueDate.Text = value
         End Set
     End Property
+
+    ' 👇 Add this property to handle status colors
+    Private _Status As String
+    Public Property Status As String
+        Get
+            Return _Status
+        End Get
+        Set(value As String)
+            _Status = value
+            ApplyStatusColor(value)
+        End Set
+    End Property
+
+    ' 👇 Function to apply border and background colors
+    Private Sub ApplyStatusColor(status As String)
+        Select Case status.ToLower()
+            Case "overdue"
+                Me.BackColor = Color.FromArgb(255, 230, 230) ' light red
+                Me.BorderStyle = BorderStyle.FixedSingle
+                Me.Padding = New Padding(2)
+                Me.ForeColor = Color.Black
+                Me.BackColor = Color.FromArgb(255, 230, 230)
+                Me.Refresh()
+                Me.BorderColor(Color.Red)
+
+            Case "due soon"
+                Me.BackColor = Color.FromArgb(255, 255, 200) ' light yellow
+                Me.BorderColor(Color.Goldenrod)
+
+            Case "due today"
+                Me.BackColor = Color.FromArgb(220, 235, 255) ' light blue
+                Me.BorderColor(Color.DodgerBlue)
+
+            Case "returned"
+                Me.BackColor = Color.FromArgb(220, 255, 220) ' light green
+                Me.BorderColor(Color.ForestGreen)
+        End Select
+    End Sub
+
+    ' Optional helper if you’re using Guna2Panel or want border styling manually
+    Private Sub BorderColor(clr As Color)
+        Me.BackColor = Me.BackColor ' just to trigger redraw
+        Me.CreateGraphics().DrawRectangle(New Pen(clr, 2), 0, 0, Me.Width - 1, Me.Height - 1)
+    End Sub
+
 End Class
