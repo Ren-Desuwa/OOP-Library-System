e47b4de3ded7f1cdc07b5381aac929fb3e94f406
Shan2411 <shandavid2411@gmail.com>
2025-11-05 02:25:58 +0800

    important commit

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -143,6 +143,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property OOP_DESIGN() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("OOP DESIGN", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property pfp() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("pfp", resourceCulture)
