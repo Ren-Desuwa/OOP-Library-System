bbc707899e450f7f668bedae0ce4cd3e40a5e963
Shan2411 <shandavid2411@gmail.com>
2025-11-05 23:52:59 +0800

    datagrid version

--- Forms/Admin-Librarian Interface/UC/UC_HPAL_Logs_Tab.Designer.vb
+++ Forms/Admin-Librarian Interface/UC/UC_HPAL_Logs_Tab.Designer.vb
@@ -23,81 +23,22 @@ Partial Class UC_HPAL_Logs_Tab
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.pnl_UC_container = New Guna.UI2.WinForms.Guna2CustomGradientPanel()
-        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
-        Me.lblDateTime = New Guna.UI2.WinForms.Guna2HtmlLabel()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.lblDateTime = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.DataGridLogs = New System.Windows.Forms.DataGridView()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.genre_panel = New Guna.UI2.WinForms.Guna2CustomGradientPanel()
         Me.TableLayoutPanel1.SuspendLayout()
-        Me.pnl_UC_container.SuspendLayout()
-        Me.TableLayoutPanel2.SuspendLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridLogs, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.genre_panel.SuspendLayout()
         Me.SuspendLayout()
         '
-        'TableLayoutPanel1
+        'Timer1
         '
-        Me.TableLayoutPanel1.BackColor = System.Drawing.Color.Transparent
-        Me.TableLayoutPanel1.ColumnCount = 3
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 2.469136!))
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 96.02195!))
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1.371742!))
-        Me.TableLayoutPanel1.Controls.Add(Me.Label1, 1, 1)
-        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel2, 1, 3)
-        Me.TableLayoutPanel1.Controls.Add(Me.DataGridView1, 1, 2)
-        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
-        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
-        Me.TableLayoutPanel1.RowCount = 4
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.793451!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.060453!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70.52897!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.36524!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(729, 397)
-        Me.TableLayoutPanel1.TabIndex = 17
-        '
-        'Label1
-        '
-        Me.Label1.BackColor = System.Drawing.Color.Transparent
-        Me.Label1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.ForeColor = System.Drawing.Color.Black
-        Me.Label1.Location = New System.Drawing.Point(20, 23)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(696, 32)
-        Me.Label1.TabIndex = 15
-        Me.Label1.Text = "Admin Logs"
-        '
-        'pnl_UC_container
-        '
-        Me.pnl_UC_container.Controls.Add(Me.TableLayoutPanel1)
-        Me.pnl_UC_container.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.pnl_UC_container.FillColor = System.Drawing.Color.FromArgb(CType(CType(251, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(232, Byte), Integer))
-        Me.pnl_UC_container.FillColor2 = System.Drawing.Color.FromArgb(CType(CType(239, Byte), Integer), CType(CType(231, Byte), Integer), CType(CType(216, Byte), Integer))
-        Me.pnl_UC_container.FillColor3 = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(212, Byte), Integer), CType(CType(192, Byte), Integer))
-        Me.pnl_UC_container.FillColor4 = System.Drawing.Color.FromArgb(CType(CType(221, Byte), Integer), CType(CType(195, Byte), Integer), CType(CType(165, Byte), Integer))
-        Me.pnl_UC_container.Location = New System.Drawing.Point(0, 0)
-        Me.pnl_UC_container.Margin = New System.Windows.Forms.Padding(2)
-        Me.pnl_UC_container.Name = "pnl_UC_container"
-        Me.pnl_UC_container.Size = New System.Drawing.Size(729, 397)
-        Me.pnl_UC_container.TabIndex = 18
-        '
-        'TableLayoutPanel2
-        '
-        Me.TableLayoutPanel2.ColumnCount = 2
-        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 7.060519!))
-        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 92.93948!))
-        Me.TableLayoutPanel2.Controls.Add(Me.lblDateTime, 1, 0)
-        Me.TableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TableLayoutPanel2.Location = New System.Drawing.Point(21, 338)
-        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
-        Me.TableLayoutPanel2.RowCount = 2
-        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 62.5!))
-        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 37.5!))
-        Me.TableLayoutPanel2.Size = New System.Drawing.Size(694, 56)
-        Me.TableLayoutPanel2.TabIndex = 17
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
         '
         'lblDateTime
         '
@@ -111,44 +52,101 @@ Partial Class UC_HPAL_Logs_Tab
         Me.lblDateTime.TabIndex = 0
         Me.lblDateTime.Text = "-"
         '
-        'Timer1
+        'TableLayoutPanel1
         '
-        Me.Timer1.Enabled = True
-        Me.Timer1.Interval = 1000
+        Me.TableLayoutPanel1.BackColor = System.Drawing.Color.Transparent
+        Me.TableLayoutPanel1.ColumnCount = 3
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1.646091!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 96.84499!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1.373626!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Label1, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel2, 1, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.DataGridLogs, 1, 1)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 3
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.060453!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 87.90932!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.282116!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(729, 397)
+        Me.TableLayoutPanel1.TabIndex = 17
         '
-        'DataGridView1
+        'DataGridLogs
         '
-        Me.DataGridView1.BackgroundColor = System.Drawing.Color.FloralWhite
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.DataGridView1.Location = New System.Drawing.Point(21, 58)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.AntiqueWhite
-        Me.DataGridView1.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black
-        Me.DataGridView1.Size = New System.Drawing.Size(694, 274)
-        Me.DataGridView1.TabIndex = 18
+        Me.DataGridLogs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridLogs.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridLogs.Location = New System.Drawing.Point(15, 34)
+        Me.DataGridLogs.Name = "DataGridLogs"
+        Me.DataGridLogs.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.White
+        Me.DataGridLogs.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black
+        Me.DataGridLogs.Size = New System.Drawing.Size(700, 342)
+        Me.DataGridLogs.TabIndex = 18
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.ColumnCount = 2
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 7.060519!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 92.93948!))
+        Me.TableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(15, 382)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 2
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 62.5!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 37.5!))
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(700, 12)
+        Me.TableLayoutPanel2.TabIndex = 17
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label1.Font = New System.Drawing.Font("Consolas", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(14, 0)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(702, 31)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "Admin Logs"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'genre_panel
+        '
+        Me.genre_panel.Controls.Add(Me.TableLayoutPanel1)
+        Me.genre_panel.CustomizableEdges.BottomRight = False
+        Me.genre_panel.CustomizableEdges.TopRight = False
+        Me.genre_panel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.genre_panel.FillColor = System.Drawing.Color.FromArgb(CType(CType(251, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(232, Byte), Integer))
+        Me.genre_panel.FillColor2 = System.Drawing.Color.FromArgb(CType(CType(239, Byte), Integer), CType(CType(231, Byte), Integer), CType(CType(216, Byte), Integer))
+        Me.genre_panel.FillColor3 = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(212, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.genre_panel.FillColor4 = System.Drawing.Color.FromArgb(CType(CType(221, Byte), Integer), CType(CType(195, Byte), Integer), CType(CType(165, Byte), Integer))
+        Me.genre_panel.Location = New System.Drawing.Point(0, 0)
+        Me.genre_panel.Margin = New System.Windows.Forms.Padding(2)
+        Me.genre_panel.Name = "genre_panel"
+        Me.genre_panel.Size = New System.Drawing.Size(729, 397)
+        Me.genre_panel.TabIndex = 18
         '
         'UC_HPAL_Logs_Tab
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Controls.Add(Me.pnl_UC_container)
+        Me.Controls.Add(Me.genre_panel)
         Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "UC_HPAL_Logs_Tab"
         Me.Size = New System.Drawing.Size(729, 397)
         Me.TableLayoutPanel1.ResumeLayout(False)
-        Me.pnl_UC_container.ResumeLayout(False)
-        Me.TableLayoutPanel2.ResumeLayout(False)
-        Me.TableLayoutPanel2.PerformLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridLogs, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.genre_panel.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents lblDateTime As Guna.UI2.WinForms.Guna2HtmlLabel
     Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
     Friend WithEvents Label1 As Label
-    Friend WithEvents pnl_UC_container As Guna.UI2.WinForms.Guna2CustomGradientPanel
     Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
-    Friend WithEvents lblDateTime As Guna.UI2.WinForms.Guna2HtmlLabel
-    Friend WithEvents Timer1 As Timer
-    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents DataGridLogs As DataGridView
+    Friend WithEvents genre_panel As Guna.UI2.WinForms.Guna2CustomGradientPanel
 End Class
