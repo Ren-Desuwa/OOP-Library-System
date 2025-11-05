25aa4158ef05ce6980a3f8836819dbe589521796
Ren-Desuwa <125685532+Ren-Desuwa@users.noreply.github.com>
2025-11-05 23:44:17 +0800

    announcement and book cover addition

--- Forms/Student Interface/UC Student/UC_announcementBox.vb
+++ Forms/Student Interface/UC Student/UC_announcementBox.vb
@@ -1,3 +1,18 @@
-﻿Public Class UC_announcementBox
+﻿' (Modified)
+Imports Classes.Models
+
+Public Class UC_announcementBox
+
+    ''' <summary>
+    ''' Populates the user control's fields with data from an Announcement object.
+    ''' </summary>
+    ''' <param name="announcement">The Announcement object containing the data.</param>
+    Public Sub Populate(ByVal announcement As Announcement)
+        ' Assuming your controls are named Title, Message, and DatePosted
+        ' (based on your original code in UC_HPS_home_tab.vb)
+        Me.Title.Text = announcement.Title           '
+        Me.Message.Text = announcement.Message         '
+        Me.DatePosted.Text = announcement.DatePosted.ToShortDateString() '
+    End Sub
 
 End Class
