a55e01ebc315c55a07ac61476aa2e20be48c1d1e
Ren-Desuwa <125685532+Ren-Desuwa@users.noreply.github.com>
2025-11-06 06:48:23 +0800

    eme

--- Forms/Admin-Librarian Interface/UC/UC_HPAL_Librarian_Tab.vb
+++ Forms/Admin-Librarian Interface/UC/UC_HPAL_Librarian_Tab.vb
@@ -1,5 +1,5 @@
 ﻿Public Class UC_HPAL_Librarian_Tab
-    Private Sub lbl_username_Click(sender As Object, e As EventArgs) Handles lbl_username.Click
+    'Private Sub lbl_username_Click(sender As Object, e As EventArgs) Handles lbl_username.Click
 
-    End Sub
+    'End Sub
 End Class
