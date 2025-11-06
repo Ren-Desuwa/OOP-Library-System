60c2192d7a85d17ff6979a579bb871d74b5aa8cc
wionquintela <wionquintela06@gmail.com>
2025-11-06 18:55:01 +0800

    UPDATED WION Branch UserRequest and BorrowRequest UserControls (fixed rename of duplicate files)

--- Forms/Admin-Librarian Interface/UC/UC_UserRequest_Container.Designer.vb
+++ Forms/Admin-Librarian Interface/UC/UC_UserRequest_Container.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class UC_User_Container
+Partial Class UC_UserRequest_Container
     Inherits System.Windows.Forms.UserControl
 
     'UserControl overrides dispose to clean up the component list.
