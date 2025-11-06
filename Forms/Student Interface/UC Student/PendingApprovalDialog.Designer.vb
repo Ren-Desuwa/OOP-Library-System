c31c390c2d93edcafed3b73a531ae46575c1dd1b
Degenerate1456 <186584961+Huuujji@users.noreply.github.com>
2025-11-07 04:41:07 +0800

    working on Approve Request of Registration of user

---
+++ Forms/Student Interface/UC Student/PendingApprovalDialog.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class PendingApprovalDialog
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Guna2CustomGradientPanel1 = New Guna.UI2.WinForms.Guna2CustomGradientPanel()
+        Me.btnConfirm = New Guna.UI2.WinForms.Guna2Button()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.StatusPollTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.Guna2CustomGradientPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Guna2CustomGradientPanel1
+        '
+        Me.Guna2CustomGradientPanel1.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2CustomGradientPanel1.Controls.Add(Me.btnConfirm)
+        Me.Guna2CustomGradientPanel1.Controls.Add(Me.lblStatus)
+        Me.Guna2CustomGradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Guna2CustomGradientPanel1.FillColor = System.Drawing.Color.FromArgb(CType(CType(250, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(230, Byte), Integer))
+        Me.Guna2CustomGradientPanel1.FillColor2 = System.Drawing.Color.FromArgb(CType(CType(240, Byte), Integer), CType(CType(229, Byte), Integer), CType(CType(211, Byte), Integer))
+        Me.Guna2CustomGradientPanel1.FillColor3 = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(201, Byte), Integer), CType(CType(166, Byte), Integer))
+        Me.Guna2CustomGradientPanel1.FillColor4 = System.Drawing.Color.FromArgb(CType(CType(198, Byte), Integer), CType(CType(173, Byte), Integer), CType(CType(143, Byte), Integer))
+        Me.Guna2CustomGradientPanel1.ForeColor = System.Drawing.Color.Transparent
+        Me.Guna2CustomGradientPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.Guna2CustomGradientPanel1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Guna2CustomGradientPanel1.Name = "Guna2CustomGradientPanel1"
+        Me.Guna2CustomGradientPanel1.Size = New System.Drawing.Size(332, 153)
+        Me.Guna2CustomGradientPanel1.TabIndex = 9
+        '
+        'btnConfirm
+        '
+        Me.btnConfirm.BorderRadius = 20
+        Me.btnConfirm.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.btnConfirm.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.btnConfirm.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.btnConfirm.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.btnConfirm.FillColor = System.Drawing.Color.White
+        Me.btnConfirm.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConfirm.ForeColor = System.Drawing.Color.FromArgb(CType(CType(74, Byte), Integer), CType(CType(59, Byte), Integer), CType(CType(42, Byte), Integer))
+        Me.btnConfirm.Location = New System.Drawing.Point(216, 104)
+        Me.btnConfirm.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnConfirm.Name = "btnConfirm"
+        Me.btnConfirm.Size = New System.Drawing.Size(108, 38)
+        Me.btnConfirm.TabIndex = 6
+        Me.btnConfirm.Text = "Confirm"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStatus.ForeColor = System.Drawing.Color.FromArgb(CType(CType(122, Byte), Integer), CType(CType(107, Byte), Integer), CType(CType(84, Byte), Integer))
+        Me.lblStatus.Location = New System.Drawing.Point(16, 40)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(86, 25)
+        Me.lblStatus.TabIndex = 5
+        Me.lblStatus.Text = "Message"
+        '
+        'StatusPollTimer
+        '
+        '
+        'PendingApprovalDialog
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(332, 153)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Guna2CustomGradientPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "PendingApprovalDialog"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "PendingApprovalDialog"
+        Me.Guna2CustomGradientPanel1.ResumeLayout(False)
+        Me.Guna2CustomGradientPanel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Guna2CustomGradientPanel1 As Guna.UI2.WinForms.Guna2CustomGradientPanel
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents btnConfirm As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents StatusPollTimer As Timer
+End Class
---
+++ Forms/Student Interface/UC Student/PendingApprovalDialog.vb
@@ -0,0 +1,53 @@
+﻿Imports System.Windows.Forms
+
+Public Class PendingApprovalDialog
+
+    ' This ID is set by the Signup form
+    Public StudentAccountID As Integer
+
+    Private Sub PendingApprovalDialog_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' 1. Set initial button state
+        btnConfirm.Enabled = False
+        btnConfirm.Text = "Waiting for Approval"
+
+        ' 2. Configure and start the timer
+        StatusPollTimer.Interval = 5000 ' Check every 5 seconds
+        StatusPollTimer.Start()
+    End Sub
+
+    Private Async Sub StatusPollTimer_Tick(sender As Object, e As EventArgs) Handles StatusPollTimer.Tick
+        Try
+            ' 3. Check the account's active status
+            ' (We will create this IsAccountActive function in Step 2)
+            If Await Program.AuthSvc.IsAccountActive(StudentAccountID) Then
+
+                ' 4. APPROVED! Stop the timer and update the UI
+                StatusPollTimer.Stop()
+
+                lblStatus.Text = "Your account has been approved! You can now log in."
+                btnConfirm.Enabled = True
+                btnConfirm.Text = "Confirm"
+
+                ' Optional: Flash the window to get the user's attention
+                Me.FlashWindow()
+            End If
+            ' 5. If not approved, the timer just ticks again
+        Catch ex As Exception
+            ' Stop the timer if a database error occurs
+            StatusPollTimer.Stop()
+            lblStatus.Text = "Error checking status. Please restart the application."
+        End Try
+    End Sub
+
+    Private Sub btnConfirm_Click(sender As Object, e As EventArgs) Handles btnConfirm.Click
+        ' 6. Close the dialog
+        Me.DialogResult = DialogResult.OK
+        Me.Close()
+    End Sub
+
+    ' Helper function to flash the window
+    Private Sub FlashWindow()
+        ' (This requires Imports System.Runtime.InteropServices)
+        ' You can skip this if it's too complex, but it's a nice touch.
+    End Sub
+End Class
