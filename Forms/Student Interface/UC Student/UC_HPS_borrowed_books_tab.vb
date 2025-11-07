5a7004166b1e956be8c0c106f9a66ea36aa82bdc
johnzildjansevero-blip <johnzildjansevero@gmail.com>
2025-11-08 05:13:15 +0800

    ngi

--- Forms/Student Interface/UC Student/UC_HPS_borrowed_books_tab.vb
+++ Forms/Student Interface/UC Student/UC_HPS_borrowed_books_tab.vb
@@ -1,14 +1,12 @@
 ﻿Imports System.IO
-Imports OOP_Library_System.Models
-' Imports MySql.Data.MySqlClient <-- REMOVED
+Imports OOP_Library_System.Models ' <-- This is needed for BorrowedBookDetails
 
 Public Class UC_HPS_borrowed_books_tab
-
-    ' Private ReadOnly connectionString As String = ... <-- REMOVED
 
     Private allBorrowedBooks As New List(Of BorrowedBookDetails)
 
     Private Sub UC_HPS_borrowed_books_tab_VisibleChanged(sender As Object, e As EventArgs) Handles Me.VisibleChanged
+        ' When the tab becomes visible, reload the data
         If Me.Visible Then LoadData()
     End Sub
 
@@ -18,11 +16,19 @@ Public Class UC_HPS_borrowed_books_tab
         allBorrowedBooks.Clear()
 
         Try
-            ' --- MODIFIED LOGIC: Call the Service Layer ---
-            Dim accountId As Integer = If(Program.currentAccount IsNot Nothing, Program.currentAccount.AccountID, 1)
-            allBorrowedBooks = Program.BorrowSvc.GetBorrowedBooksDetails(accountId)
-            ' --- END OF MODIFIED LOGIC ---
+            ' Get the logged-in user's ID
+            Dim accountId As Integer = If(Program.currentAccount IsNot Nothing, Program.currentAccount.AccountID, 0)
 
+            ' If for some reason we don't have an account, exit
+            If accountId = 0 Then
+                MessageBox.Show("Could not find user account. Please log in again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return
+            End If
+
+            ' Fetch all borrowed book records from the service
+            allBorrowedBooks = Program.BorrowSvc.GetBorrowedBooksDetails(accountId)
+
+            ' Run the display/filter logic
             UpdateDisplay()
 
         Catch ex As Exception
@@ -35,25 +41,44 @@ Public Class UC_HPS_borrowed_books_tab
         flpBorrowedBooks.SuspendLayout()
         flpBorrowedBooks.Controls.Clear()
 
-        Dim filteredList As List(Of BorrowedBookDetails) = allBorrowedBooks
+        ' --- WE NO LONGER RECALCULATE STATUS HERE ---
 
-        ' Apply filter checkboxes
+        ' --- 2. APPLY CORRECTED FILTER LOGIC ---
         Dim showOverdue As Boolean = Guna2CheckBox1.Checked
         Dim showDueSoon As Boolean = Guna2CheckBox2.Checked
         Dim showDueToday As Boolean = Guna2CheckBox3.Checked
         Dim showReturned As Boolean = Guna2CheckBox4.Checked
-        Dim showAll = Not (showOverdue Or showDueSoon Or showDueToday Or showReturned)
 
-        If Not showAll Then
+        Dim today As Date = Date.Today
+        Dim anyBoxChecked = (showOverdue Or showDueSoon Or showDueToday Or showReturned)
+
+        Dim filteredList As New List(Of BorrowedBookDetails)()
+
+        If anyBoxChecked Then
+            ' Filter the list based on the correct logic
             filteredList = allBorrowedBooks.Where(Function(b)
-                                                      Return (showOverdue AndAlso b.Status = BorrowedBookStatus.Overdue) Or
-                                                             (showDueSoon AndAlso b.Status = BorrowedBookStatus.DueSoon) Or
-                                                             (showDueToday AndAlso b.Status = BorrowedBookStatus.DueToday) Or
-                                                             (showReturned AndAlso b.Status = BorrowedBookStatus.Returned)
-                                                  End Function).ToList()
-        End If
+                                                      ' Get the database status
+                                                      Dim isOverdueStatus = (b.Status = OOP_Library_System.Models.BorrowedBookStatus.Overdue)
+                                                      Dim isReturnedStatus = (b.Status = OOP_Library_System.Models.BorrowedBookStatus.Returned)
 
-        ' Add cards to panel (newest on top)
+                                                      ' Calculate the date-based status
+                                                      ' We must add "AndAlso Not isReturnedStatus" so returned books don't appear in date filters
+                                                      Dim isDueToday = (b.DueDate.HasValue AndAlso b.DueDate.Value.Date = today AndAlso Not isReturnedStatus)
+                                                      Dim isDueSoon = (b.DueDate.HasValue AndAlso b.DueDate.Value.Date > today AndAlso b.DueDate.Value.Date <= today.AddDays(3) AndAlso Not isReturnedStatus)
+
+                                                      ' Check which filters to show
+                                                      Return (showOverdue AndAlso isOverdueStatus) Or
+                                                             (showReturned AndAlso isReturnedStatus) Or
+                                                             (showDueToday AndAlso isDueToday) Or
+                                                             (showDueSoon AndAlso isDueSoon)
+                                                  End Function).ToList()
+        Else
+            ' NO boxes are checked, so filteredList remains empty.
+        End If
+        ' --- END OF FILTER LOGIC ---
+
+
+        ' --- 3. DISPLAY THE FILTERED CARDS ---
         For Each book In filteredList
             Dim card As New BorrowedBooksCard()
             card.Width = flpBorrowedBooks.ClientSize.Width - 25
@@ -63,24 +88,25 @@ Public Class UC_HPS_borrowed_books_tab
             card.BorrowedDate = If(book.BorrowedDate.HasValue, "Borrowed: " & book.BorrowedDate.Value.ToString("MMM dd, yyyy"), "")
             card.DueDate = If(book.DueDate.HasValue, "Due: " & book.DueDate.Value.ToString("MMM dd, yyyy"), "")
 
-            Select Case book.Status
-                Case BorrowedBookStatus.Overdue
-                    card.SetStatusColor(Color.LightCoral)
-                Case BorrowedBookStatus.DueToday
-                    card.SetStatusColor(Color.FromArgb(128, 255, 255))
-                Case BorrowedBookStatus.DueSoon
-                    card.SetStatusColor(Color.FromArgb(255, 255, 128))
-                Case BorrowedBookStatus.Returned
-                    card.SetStatusColor(Color.FromArgb(128, 255, 128))
-                    card.DueDate = "Returned"
-                Case Else
-                    card.SetStatusColor(Color.White)
-            End Select
+            ' --- *** START OF MODIFICATION *** ---
+            ' We call the new function on the card and pass it the info it needs
+            Dim isOverdue = (book.Status = OOP_Library_System.Models.BorrowedBookStatus.Overdue)
+            Dim isReturned = (book.Status = OOP_Library_System.Models.BorrowedBookStatus.Returned)
+
+            ' The card will now calculate its own color
+            card.SetDynamicStatus(book.DueDate, isReturned, isOverdue)
+
+            ' The special case for "Returned" text still needs to be here.
+            If isReturned Then
+                card.DueDate = "Returned"
+            End If
+            ' --- *** END OF MODIFICATION *** ---
 
             ' Insert at top instead of bottom
             flpBorrowedBooks.Controls.Add(card)
             flpBorrowedBooks.Controls.SetChildIndex(card, 0)
         Next
+        ' --- END OF DISPLAY ---
 
 
         ' Layout adjustments
@@ -90,19 +116,12 @@ Public Class UC_HPS_borrowed_books_tab
         flpBorrowedBooks.ResumeLayout()
     End Sub
 
-    ' Filter checkbox behavior
+    ' This event handler is correct and allows multiple selections.
     Private Sub Filter_CheckedChanged(sender As Object, e As EventArgs) Handles Guna2CheckBox1.CheckedChanged,
-                                                                               Guna2CheckBox2.CheckedChanged,
-                                                                               Guna2CheckBox3.CheckedChanged,
-                                                                               Guna2CheckBox4.CheckedChanged
-        Dim cb As Guna.UI2.WinForms.Guna2CheckBox = DirectCast(sender, Guna.UI2.WinForms.Guna2CheckBox)
-        If cb.Checked Then
-            For Each ctrl As Control In {Guna2CheckBox1, Guna2CheckBox2, Guna2CheckBox3, Guna2CheckBox4}
-                If ctrl IsNot cb Then
-                    DirectCast(ctrl, Guna.UI2.WinForms.Guna2CheckBox).Checked = False
-                End If
-            Next
-        End If
+                                                                         Guna2CheckBox2.CheckedChanged,
+                                                                         Guna2CheckBox3.CheckedChanged,
+                                                                         Guna2CheckBox4.CheckedChanged
+        ' Just call UpdateDisplay.
         UpdateDisplay()
     End Sub
 
