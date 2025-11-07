b2e7e95bc3699706c2f372f0b3a3f0daaa536d15
Shan2411 <shandavid2411@gmail.com>
2025-11-08 03:57:24 +0800

    pushhhhhhhhhhhhhhh

--- Forms/Admin-Librarian Interface/UC/LibrarianADD.Designer.vb
+++ Forms/Admin-Librarian Interface/UC/LibrarianADD.Designer.vb
@@ -42,6 +42,10 @@ Partial Class LibrarianADD
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.btn_sendcode = New Guna.UI2.WinForms.Guna2Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.otpTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.Label7 = New System.Windows.Forms.Label()
         Me.TableLayoutPanel4.SuspendLayout()
         Me.TableLayoutPanel5.SuspendLayout()
         Me.TableLayoutPanel8.SuspendLayout()
@@ -74,7 +78,7 @@ Partial Class LibrarianADD
         Me.TableLayoutPanel4.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
         Me.TableLayoutPanel4.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
         Me.TableLayoutPanel4.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
-        Me.TableLayoutPanel4.Size = New System.Drawing.Size(800, 450)
+        Me.TableLayoutPanel4.Size = New System.Drawing.Size(837, 458)
         Me.TableLayoutPanel4.TabIndex = 1
         '
         'TableLayoutPanel5
@@ -90,7 +94,7 @@ Partial Class LibrarianADD
         Me.TableLayoutPanel5.RowCount = 1
         Me.TableLayoutPanel5.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
         Me.TableLayoutPanel5.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
-        Me.TableLayoutPanel5.Size = New System.Drawing.Size(750, 265)
+        Me.TableLayoutPanel5.Size = New System.Drawing.Size(787, 270)
         Me.TableLayoutPanel5.TabIndex = 0
         '
         'Label1
@@ -99,7 +103,7 @@ Partial Class LibrarianADD
         Me.Label1.BackColor = System.Drawing.Color.Transparent
         Me.Label1.Font = New System.Drawing.Font("Microsoft YaHei UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.Black
-        Me.Label1.Location = New System.Drawing.Point(47, 16)
+        Me.Label1.Location = New System.Drawing.Point(88, 17)
         Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(110, 25)
@@ -113,7 +117,7 @@ Partial Class LibrarianADD
         Me.Label2.BackColor = System.Drawing.Color.Transparent
         Me.Label2.Font = New System.Drawing.Font("Microsoft YaHei UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.ForeColor = System.Drawing.Color.Black
-        Me.Label2.Location = New System.Drawing.Point(35, 59)
+        Me.Label2.Location = New System.Drawing.Point(76, 60)
         Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(123, 25)
@@ -127,7 +131,7 @@ Partial Class LibrarianADD
         Me.Label3.BackColor = System.Drawing.Color.Transparent
         Me.Label3.Font = New System.Drawing.Font("Microsoft YaHei UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label3.ForeColor = System.Drawing.Color.Black
-        Me.Label3.Location = New System.Drawing.Point(83, 102)
+        Me.Label3.Location = New System.Drawing.Point(124, 103)
         Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(75, 25)
@@ -141,7 +145,7 @@ Partial Class LibrarianADD
         Me.Label6.BackColor = System.Drawing.Color.Transparent
         Me.Label6.Font = New System.Drawing.Font("Microsoft YaHei UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label6.ForeColor = System.Drawing.Color.Black
-        Me.Label6.Location = New System.Drawing.Point(54, 188)
+        Me.Label6.Location = New System.Drawing.Point(95, 208)
         Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(104, 25)
@@ -156,12 +160,12 @@ Partial Class LibrarianADD
         Me.TableLayoutPanel8.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
         Me.TableLayoutPanel8.Controls.Add(Me.TableLayoutPanel10, 1, 0)
         Me.TableLayoutPanel8.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TableLayoutPanel8.Location = New System.Drawing.Point(5, 406)
+        Me.TableLayoutPanel8.Location = New System.Drawing.Point(5, 413)
         Me.TableLayoutPanel8.Name = "TableLayoutPanel8"
         Me.TableLayoutPanel8.RowCount = 1
         Me.TableLayoutPanel8.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
         Me.TableLayoutPanel8.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.TableLayoutPanel8.Size = New System.Drawing.Size(790, 39)
+        Me.TableLayoutPanel8.Size = New System.Drawing.Size(827, 40)
         Me.TableLayoutPanel8.TabIndex = 1
         '
         'TableLayoutPanel10
@@ -172,11 +176,11 @@ Partial Class LibrarianADD
         Me.TableLayoutPanel10.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45.0!))
         Me.TableLayoutPanel10.Controls.Add(Me.Guna2GradientButton3, 2, 0)
         Me.TableLayoutPanel10.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TableLayoutPanel10.Location = New System.Drawing.Point(398, 3)
+        Me.TableLayoutPanel10.Location = New System.Drawing.Point(416, 3)
         Me.TableLayoutPanel10.Name = "TableLayoutPanel10"
         Me.TableLayoutPanel10.RowCount = 1
         Me.TableLayoutPanel10.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.TableLayoutPanel10.Size = New System.Drawing.Size(389, 33)
+        Me.TableLayoutPanel10.Size = New System.Drawing.Size(408, 34)
         Me.TableLayoutPanel10.TabIndex = 3
         '
         'Guna2GradientButton3
@@ -193,10 +197,10 @@ Partial Class LibrarianADD
         Me.Guna2GradientButton3.FillColor2 = System.Drawing.Color.CadetBlue
         Me.Guna2GradientButton3.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Guna2GradientButton3.ForeColor = System.Drawing.Color.White
-        Me.Guna2GradientButton3.Location = New System.Drawing.Point(213, 0)
+        Me.Guna2GradientButton3.Location = New System.Drawing.Point(223, 0)
         Me.Guna2GradientButton3.Margin = New System.Windows.Forms.Padding(0)
         Me.Guna2GradientButton3.Name = "Guna2GradientButton3"
-        Me.Guna2GradientButton3.Size = New System.Drawing.Size(176, 33)
+        Me.Guna2GradientButton3.Size = New System.Drawing.Size(185, 34)
         Me.Guna2GradientButton3.TabIndex = 10
         Me.Guna2GradientButton3.Text = "Add Librarian"
         '
@@ -209,7 +213,7 @@ Partial Class LibrarianADD
         Me.Guna2PictureBox1.ImageRotate = 0!
         Me.Guna2PictureBox1.Location = New System.Drawing.Point(3, 3)
         Me.Guna2PictureBox1.Name = "Guna2PictureBox1"
-        Me.Guna2PictureBox1.Size = New System.Drawing.Size(223, 259)
+        Me.Guna2PictureBox1.Size = New System.Drawing.Size(234, 264)
         Me.Guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.Guna2PictureBox1.TabIndex = 2
         Me.Guna2PictureBox1.TabStop = False
@@ -222,7 +226,7 @@ Partial Class LibrarianADD
         Me.Guna2GradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Guna2GradientPanel1.Location = New System.Drawing.Point(5, 5)
         Me.Guna2GradientPanel1.Name = "Guna2GradientPanel1"
-        Me.Guna2GradientPanel1.Size = New System.Drawing.Size(790, 393)
+        Me.Guna2GradientPanel1.Size = New System.Drawing.Size(827, 400)
         Me.Guna2GradientPanel1.TabIndex = 2
         '
         'Label4
@@ -251,6 +255,9 @@ Partial Class LibrarianADD
         '
         'Guna2Panel1
         '
+        Me.Guna2Panel1.Controls.Add(Me.TextBox5)
+        Me.Guna2Panel1.Controls.Add(Me.Label7)
+        Me.Guna2Panel1.Controls.Add(Me.btn_sendcode)
         Me.Guna2Panel1.Controls.Add(Me.TextBox4)
         Me.Guna2Panel1.Controls.Add(Me.TextBox3)
         Me.Guna2Panel1.Controls.Add(Me.TextBox2)
@@ -260,15 +267,15 @@ Partial Class LibrarianADD
         Me.Guna2Panel1.Controls.Add(Me.Label2)
         Me.Guna2Panel1.Controls.Add(Me.Label3)
         Me.Guna2Panel1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Guna2Panel1.Location = New System.Drawing.Point(232, 3)
+        Me.Guna2Panel1.Location = New System.Drawing.Point(243, 3)
         Me.Guna2Panel1.Name = "Guna2Panel1"
-        Me.Guna2Panel1.Size = New System.Drawing.Size(515, 259)
+        Me.Guna2Panel1.Size = New System.Drawing.Size(541, 264)
         Me.Guna2Panel1.TabIndex = 3
         '
         'TextBox1
         '
         Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox1.Location = New System.Drawing.Point(175, 189)
+        Me.TextBox1.Location = New System.Drawing.Point(216, 209)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(234, 26)
         Me.TextBox1.TabIndex = 24
@@ -276,7 +283,7 @@ Partial Class LibrarianADD
         'TextBox2
         '
         Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox2.Location = New System.Drawing.Point(175, 101)
+        Me.TextBox2.Location = New System.Drawing.Point(216, 102)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(234, 26)
         Me.TextBox2.TabIndex = 25
@@ -284,7 +291,7 @@ Partial Class LibrarianADD
         'TextBox3
         '
         Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox3.Location = New System.Drawing.Point(175, 61)
+        Me.TextBox3.Location = New System.Drawing.Point(216, 62)
         Me.TextBox3.Name = "TextBox3"
         Me.TextBox3.Size = New System.Drawing.Size(234, 26)
         Me.TextBox3.TabIndex = 26
@@ -292,17 +299,64 @@ Partial Class LibrarianADD
         'TextBox4
         '
         Me.TextBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox4.Location = New System.Drawing.Point(175, 17)
+        Me.TextBox4.Location = New System.Drawing.Point(216, 18)
         Me.TextBox4.Name = "TextBox4"
         Me.TextBox4.Size = New System.Drawing.Size(234, 26)
         Me.TextBox4.TabIndex = 27
+        '
+        'btn_sendcode
+        '
+        Me.btn_sendcode.BorderColor = System.Drawing.Color.Transparent
+        Me.btn_sendcode.BorderRadius = 10
+        Me.btn_sendcode.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.btn_sendcode.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.btn_sendcode.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.btn_sendcode.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.btn_sendcode.FillColor = System.Drawing.Color.Transparent
+        Me.btn_sendcode.Font = New System.Drawing.Font("Segoe UI", 7.8!)
+        Me.btn_sendcode.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.btn_sendcode.Location = New System.Drawing.Point(268, 179)
+        Me.btn_sendcode.Margin = New System.Windows.Forms.Padding(2)
+        Me.btn_sendcode.Name = "btn_sendcode"
+        Me.btn_sendcode.Size = New System.Drawing.Size(126, 20)
+        Me.btn_sendcode.TabIndex = 28
+        Me.btn_sendcode.Text = "Send Code"
+        Me.btn_sendcode.Visible = False
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox5.Location = New System.Drawing.Point(216, 148)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(234, 26)
+        Me.TextBox5.TabIndex = 30
+        Me.TextBox5.Visible = False
+        '
+        'otpTimer
+        '
+        Me.otpTimer.Interval = 1000
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.Color.Transparent
+        Me.Label7.Font = New System.Drawing.Font("Microsoft YaHei UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.Black
+        Me.Label7.Location = New System.Drawing.Point(30, 147)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(176, 25)
+        Me.Label7.TabIndex = 29
+        Me.Label7.Text = "Verification Code:"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Label7.Visible = False
         '
         'LibrarianADD
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.OldLace
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(837, 458)
         Me.Controls.Add(Me.TableLayoutPanel4)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "LibrarianADD"
@@ -339,4 +393,8 @@ Partial Class LibrarianADD
     Friend WithEvents TextBox3 As TextBox
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents btn_sendcode As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents otpTimer As Timer
+    Friend WithEvents Label7 As Label
 End Class
